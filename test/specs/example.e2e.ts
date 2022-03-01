7834c6972fe3ddde7eaccee41179d80be29fd8eb
agustin <agustincatalano@gmail.com>
2022-03-01 16:23:16 +0100

    config allure and eslint - checkboxes test added

--- test/specs/example.e2e.ts
+++ test/specs/example.e2e.ts
@@ -1,14 +1,28 @@
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await browser.url('https://the-internet.herokuapp.com/login');
+  it('should login with valid credentials', async () => {
+    await browser.url('/login')
 
-        await $('#username').setValue('tomsmith');
-        await $('#password').setValue('SuperSecretPassword!');
-        await $('button[type="submit"]').click();
+    await $('#username').setValue('tomsmith')
+    await $('#password').setValue('SuperSecretPassword!')
+    await $('button[type="submit"]').click()
 
-        await expect($('#flash')).toBeExisting();
-        await expect($('#flash')).toHaveTextContaining(
-            'You logged into a secure area!');
-    });
-});
+    await expect($('#flash')).toBeExisting()
+    await expect($('#flash')).toHaveTextContaining(
+      'You logged into a secure area!'
+    )
+  })
+})
 
+describe('Checkboxes test', () => {
+  it('should validate checkboxes ', async () => {
+    const cb1 = await $('#checkboxes input:first-Child')
+    const cb2 = await $('#checkboxes input:last-Child')
+    await browser.url('/checkboxes')
+    await expect($('#checkboxes')).toBeDisplayed()
+    await expect(cb1).not.toBeSelected()
+    await expect(cb2).toBeSelected()
+    await cb1.click()
+    await expect(cb1).toBeSelected()
+    await expect(cb2).toBeSelected()
+  })
+})
